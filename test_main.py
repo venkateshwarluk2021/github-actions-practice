2bd0a1b4056b02b6fc72d8e8d92caf22d94cd0c7
venkateshwarluk2021 <kvenkateshwarlu.2010@gmail.com>
2025-06-04 21:06:35 +0530

    flake is sensitive 3

--- test_main.py
+++ test_main.py
@@ -2,10 +2,10 @@ from main import add, subtract
 
 
 def test_add():
-    assert add(2, 3)==5
-    assert add(1, -1)==0
+    assert add(2, 3) == 5
+    assert add(1, -1) == 0
 
 
 def test_subtract():
-    assert subtract(11, 3)==8
-    assert subtract(5, -5)==10
+    assert subtract(11, 3) == 8
+    assert subtract(5, -5) == 10
